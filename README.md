eaa3ba860e666467361a6b97aad94f1f51ce697f
Mauricio Soares da Silva <mauri@DESKTOP-J3UEJ78>
2017-03-04 16:12:53 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,41 @@
+# Delete Tomcat Files Plugin
+
+Delete temporary Tomcat files plugin for `NetBeans IDE 8.2+`.
+
+## Feature
+
+This plugin tries to clear all Tomcat temp files.
+
+## Install
+
+Go to `Tools > Plugins > Downloaded Intalled > Add Plugins...` and add the `deletetomcatfilesplugin.nbm` file.
+
+##Usage
+
+Go to `Tools > Clear Tomcat` and click.
+
+It will try to perform the following tasks:
+ - Delete all but ROOT.xml or manager.xml at: 
+  <user-dir>/AppData/Roaming/NetBeans/X.X.X/apache-tomcat-X.X.X.X_base/conf/Catalina/localhost
+ - Delete all at <user-dir>/AppData/Roaming/NetBeans/X.X.X/apache-tomcat-X.X.X.X_base/logs
+ - Delete all at <user-dir>/AppData/Roaming/NetBeans/X.X.X/apache-tomcat-X.X.X.X_base/temp
+ - Delete all at <user-dir>/AppData/Roaming/NetBeans/X.X.X/apache-tomcat-X.X.X.X_base/webapps
+ - Delete all but /ROOT or /manager at: 
+   <user-dir>/AppData/Roaming/NetBeans/X.X.X/apache-tomcat-X.X.X.X_base/work/Catalina/localhost
+
+
+## Credits
+Mauricio Soares da Silva - [maumss@users.noreply.github.com](mailto:maumss@users.noreply.github.com)
+
+## License
+
+[GNU General Public License (GPL) v3](http://www.gnu.org/licenses/)
+
+Copyright &copy; 2015 Mauricio Soares da Silva
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
---
+++ src/br/com/yahoo/mau_mss/deletetomcatfilesplugin/DeleteTempAction.java
@@ -0,0 +1,255 @@
+package br.com.yahoo.mau_mss.deletetomcatfilesplugin;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.FileFilterUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
+import org.openide.awt.StatusDisplayer;
+import org.openide.util.NbBundle.Messages;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Title: DeleteTempAction
+ * Description: This plugin will try to perform the following tasks:
+ * - Delete all but ROOT.xml or manager.xml at:
+ * C:\Users\<user-dir>\AppData\Roaming\NetBeans\X.X.X\apache-tomcat-X.X.X.X_base\conf\Catalina\localhost
+ * - Delete all at
+ * C:\Users\<user-dir>\AppData\Roaming\NetBeans\X.X.X\apache-tomcat-X.X.X.X_base\logs
+ * - Delete all at
+ * C:\Users\<user-dir>\AppData\Roaming\NetBeans\X.X.X\apache-tomcat-X.X.X.X_base\temp
+ * - Delete all at
+ * C:\Users\<user-dir>\AppData\Roaming\NetBeans\X.X.X\apache-tomcat-X.X.X.X_base\webapps
+ * - Delete all but /ROOT or /manager at:
+ * C:\Users\<user-dir>\AppData\Roaming\NetBeans\X.X.X\apache-tomcat-X.X.X.X_base\work\Catalina\localhost
+ * Date: September 23, 2014, 9:20:00 PM
+ *
+ * @author Mauricio Soares da Silva (mauricio.soares)
+ * @see https://platform.netbeans.org/tutorials/nbm-copyfqn.html
+ */
+@ActionID(
+        category = "File",
+        id = "br.com.yahoo.mau_mss.deletetomcatfilesplugin.DeleteTempAction"
+)
+@ActionRegistration(
+        iconBase = "resources/trashEmpty.png",
+        displayName = "#CTL_DeleteTempAction"
+)
+@ActionReference(path = "Menu/Tools", position = 190)
+@Messages("CTL_DeleteTempAction=Clear Tomcat")
+public class DeleteTempAction implements ActionListener {
+  private String baseDir;
+  private static final String TOMCAT_PREFIX = "apache-tomcat";
+  private static final String ROOT_NAME = "ROOT";
+  private static final String MANAGER_NAME = "manager";
+  private static final Logger logger = Logger.getLogger(DeleteTempAction.class.getName());
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    mountTomcatBase();
+    NotifyDescriptor d = new NotifyDescriptor.Confirmation(
+        String.format("Do you really want to delete Tomcat logs and temp files at %s?", this.baseDir),
+        "Confirmation", NotifyDescriptor.OK_CANCEL_OPTION);
+    if (DialogDisplayer.getDefault().notify(d) == NotifyDescriptor.CANCEL_OPTION) {
+      StatusDisplayer.getDefault().setStatusText("Operation cancelled");
+      return;
+    }
+    if (apagarConf() & apagarLogs() & apagarTemp() & apagarWebApps() & apagarWork()) {
+      StatusDisplayer.getDefault().setStatusText("Great, all Tomcat logs and temp files were deleted with success!");
+    } else {
+      StatusDisplayer.getDefault().setStatusText("Sorry, some files could not been deleted.");
+    }
+  }
+
+  private void mountTomcatBase() {
+    StringBuilder sb = new StringBuilder();
+    sb.append(SystemUtils.USER_HOME);
+    sb.append(SystemUtils.FILE_SEPARATOR);
+    sb.append("AppData");
+    sb.append(SystemUtils.FILE_SEPARATOR);
+    sb.append("Roaming");
+    sb.append(SystemUtils.FILE_SEPARATOR);
+    sb.append("NetBeans");
+    sb.append(SystemUtils.FILE_SEPARATOR);
+    sb.append(getVersaoNetBeans(sb.toString()));
+    sb.append(SystemUtils.FILE_SEPARATOR);
+    sb.append(getDirApache(sb.toString()));
+    this.baseDir = sb.toString();
+  }
+
+  private String getVersaoNetBeans(String path) {
+    File pathFile = FileUtils.getFile(path);
+    File[] folders = pathFile.listFiles((FileFilter) FileFilterUtils.directoryFileFilter());
+    if (folders == null) {
+      logger.log(Level.WARNING, "Nenhum diret\u00f3rio do NetBeans encontrado em {0}", path);
+      return "";
+    }
+    String maiorVersao = "";
+    for (File file : folders) {
+      if (file.getName().compareTo(maiorVersao) > 0) {
+        maiorVersao = file.getName();
+      }
+    }
+    return maiorVersao;
+  }
+
+  private String getDirApache(String path) {
+    File pathFile = FileUtils.getFile(path);
+    File[] folders = pathFile.listFiles((FileFilter) FileFilterUtils.directoryFileFilter());
+    if (folders == null) {
+      logger.log(Level.WARNING, "Nenhum diret\u00f3rio do apache encontrado em {0}", path);
+      return "";
+    }
+    String pathApache = "";
+    for (File file : folders) {
+      if (StringUtils.startsWithIgnoreCase(file.getName(), DeleteTempAction.TOMCAT_PREFIX)) {
+        pathApache = file.getName();
+      }
+    }
+    return pathApache;
+  }
+
+  private boolean apagarConf() {
+    boolean noErrorsFounde = true;
+    for (File file : findXMLFiles(getConfDir())) {
+      if (!StringUtils.startsWithIgnoreCase(file.getName(), DeleteTempAction.MANAGER_NAME)
+              && !StringUtils.startsWithIgnoreCase(file.getName(), DeleteTempAction.ROOT_NAME)) {
+        logger.log(Level.INFO, "Apagando o arquivo {0}", file.getAbsolutePath());
+        if (!FileUtils.deleteQuietly(file)) {
+          noErrorsFounde = false;
+        }
+      }
+    }
+    return noErrorsFounde;
+  }
+
+  private Collection<File> findXMLFiles(String path) {
+    final String[] SUFFIX = {"xml"};
+    File pathFile = FileUtils.getFile(path);
+    return FileUtils.listFiles(pathFile, SUFFIX, false);
+  }
+
+  private String getConfDir() {
+    StringBuilder sb = new StringBuilder();
+    sb.append(this.baseDir);
+    sb.append(SystemUtils.FILE_SEPARATOR);
+    sb.append("conf");
+    sb.append(SystemUtils.FILE_SEPARATOR);
+    sb.append("Catalina");
+    sb.append(SystemUtils.FILE_SEPARATOR);
+    sb.append("localhost");
+    return sb.toString();
+  }
+
+  private boolean apagarLogs() {
+    boolean noErrorsFounde = true;
+    for (File file : findLogFiles(getLogDir())) {
+      logger.log(Level.INFO, "Apagando o arquivo {0}", file.getAbsolutePath());
+      if (!FileUtils.deleteQuietly(file)) {
+        noErrorsFounde = false;
+      }
+    }
+    return noErrorsFounde;
+  }
+
+  private Collection<File> findLogFiles(String path) {
+    final String[] SUFFIX = {"log", "txt"};
+    File pathFile = FileUtils.getFile(path);
+    return FileUtils.listFiles(pathFile, SUFFIX, false);
+  }
+
+  private String getLogDir() {
+    StringBuilder sb = new StringBuilder();
+    sb.append(this.baseDir);
+    sb.append(SystemUtils.FILE_SEPARATOR);
+    sb.append("logs");
+    return sb.toString();
+  }
+
+  private boolean apagarTemp() {
+    boolean noErrorsFounde = true;
+    for (File file : findAllFiles(getTempDir())) {
+      logger.log(Level.INFO, "Apagando o arquivo {0}", file.getAbsolutePath());
+      if (!FileUtils.deleteQuietly(file)) {
+        noErrorsFounde = false;
+      }
+    }
+    return noErrorsFounde;
+  }
+
+  private String getTempDir() {
+    StringBuilder sb = new StringBuilder();
+    sb.append(this.baseDir);
+    sb.append(SystemUtils.FILE_SEPARATOR);
+    sb.append("temp");
+    return sb.toString();
+  }
+
+  private boolean apagarWebApps() {
+    boolean noErrorsFounde = true;
+    for (File file : findAllFiles(getWebAppsDir())) {
+      logger.log(Level.INFO, "Apagando o arquivo {0}", file.getAbsolutePath());
+      if (!FileUtils.deleteQuietly(file)) {
+        noErrorsFounde = false;
+      }
+    }
+    return noErrorsFounde;
+  }
+
+  private String getWebAppsDir() {
+    StringBuilder sb = new StringBuilder();
+    sb.append(this.baseDir);
+    sb.append(SystemUtils.FILE_SEPARATOR);
+    sb.append("webapps");
+    return sb.toString();
+  }
+
+  private Collection<File> findAllFiles(String path) {
+    File pathFile = FileUtils.getFile(path);
+    return FileUtils.listFiles(pathFile, null, false);
+  }
+
+  private boolean apagarWork() {
+    File pathFile = FileUtils.getFile(getWorkDir());
+    File[] folders = pathFile.listFiles((FileFilter) FileFilterUtils.directoryFileFilter());
+    boolean noErrorsFounde = true;
+    if (folders == null) {
+      logger.log(Level.INFO, "Nenhum diret\u00f3rio em {0} a apagar", pathFile.getAbsolutePath());
+      return noErrorsFounde;
+    }
+    for (File file : folders) {
+      if (!StringUtils.startsWithIgnoreCase(file.getName(), DeleteTempAction.MANAGER_NAME)
+              && !StringUtils.startsWithIgnoreCase(file.getName(), DeleteTempAction.ROOT_NAME)) {
+        logger.log(Level.INFO, "Apagando diret\u00f3rio {0}", file.getAbsolutePath());
+        if (!FileUtils.deleteQuietly(file)) {
+          noErrorsFounde = false;
+        }
+      }
+    }
+    return noErrorsFounde;
+  }
+
+  private String getWorkDir() {
+    StringBuilder sb = new StringBuilder();
+    sb.append(this.baseDir);
+    sb.append(SystemUtils.FILE_SEPARATOR);
+    sb.append("work");
+    sb.append(SystemUtils.FILE_SEPARATOR);
+    sb.append("Catalina");
+    sb.append(SystemUtils.FILE_SEPARATOR);
+    sb.append("localhost");
+    return sb.toString();
+  }
+
+}
